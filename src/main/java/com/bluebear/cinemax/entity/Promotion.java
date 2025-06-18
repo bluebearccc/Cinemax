0f807b2d9bf5e53ec3beaa9ad14a828fa02ba746
BinhBinh13 <bdang798@gmail.com>
2025-06-19 01:12:05 +0700

    relocate directory folder and reformat source code

--- src/main/java/com/bluebear/cinemax/entity/Promotion.java
+++ src/main/java/com/bluebear/cinemax/entity/Promotion.java
@@ -1,39 +1,48 @@
 package com.bluebear.cinemax.entity;
 
+import com.bluebear.cinemax.enumtype.Promotion_Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "Promotion")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Promotion {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "PromodtionID")
-    private Integer promotionId;
-    
-    @Column(name = "PromotionCode", length = 10, nullable = false, unique = true)
+    @Column(name = "PromotionID")
+    private Integer promotionID;
+
+    @Column(name = "PromotionCode", nullable = false, length = 10, unique = true)
     private String promotionCode;
-    
+
     @Column(name = "Discount", nullable = false)
-    private Integer discount;
-    
+    private Integer discount; // Giá trị giảm giá (ví dụ: phần trăm hoặc số tiền cố định)
+
     @Column(name = "StartTime", nullable = false)
     private LocalDateTime startTime;
-    
+
     @Column(name = "EndTime", nullable = false)
     private LocalDateTime endTime;
-    
+
     @Column(name = "Quantity", nullable = false)
-    private Integer quantity;
-    
-    @Column(name = "Active", length = 20, nullable = false)
-    private String active;
+    private Integer quantity; // Số lượng mã khuyến mãi có sẵn
+
+    // Sử dụng Enum để quản lý trạng thái, giúp mã sạch hơn và an toàn kiểu dữ liệu
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Status", nullable = false, length = 20)
+    private Promotion_Status status; // Enum PromotionStatus sẽ được định nghĩa bên dưới
+
+    @OneToMany(mappedBy = "promotion")
+    private List<Invoice> InvoiceList;
 }
